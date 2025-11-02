24c7a2754fb8cf1682aee65cca80795c0050fb9e
Maciej Kucharz <m@kucharz.net>
2025-11-02 19:58:24 +0100

    fix: remove invalid psql metacommand from migration file
    - Removed '\ restrict...' line that was causing syntax error
    - Fixed SET command formatting (removed unnecessary line breaks)
    - This was preventing 0001_init.sql from running successfully
    
    Fixes 'syntax error at or near "' in migration execution

--- apps/server-nest/migrations/0001_init.sql
+++ apps/server-nest/migrations/0001_init.sql
@@ -70,34 +70,21 @@
 --
 -- PostgreSQL database dump
 --
-\ restrict jKMNXMzEwfNT8vIdSxrL7R1afwOwVIa1tMGBrEBfDpbVc5DoETxpKcBjZ3Hp4nW -- Dumped from database version 16.10 (Debian 16.10-1.pgdg12+1)
+
+-- Dumped from database version 16.10 (Debian 16.10-1.pgdg12+1)
 -- Dumped by pg_dump version 16.10 (Debian 16.10-1.pgdg12+1)
-SET
-    statement_timeout = 0;
 
-SET
-    lock_timeout = 0;
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
 
-SET
-    idle_in_transaction_session_timeout = 0;
+SELECT pg_catalog.set_config('search_path', '', false);
 
-SET
-    client_encoding = 'UTF8';
-
-SET
-    standard_conforming_strings = on;
-
-SELECT
-    pg_catalog.set_config('search_path', '', false);
-
-SET
-    check_function_bodies = false;
-
-SET
-    xmloption = content;
-
-SET
-    client_min_messages = warning;
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
 
 SET
     row_security = off;
